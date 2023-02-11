12ce5586716d389071e8088d669937cec435d267
N Space <1257310+nourspace@users.noreply.github.com>
2023-02-11 15:27:08 +0100

    feat: request stats only at certain minutes
    Every 15 minutes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import timedelta, datetime
 from os import environ
 
 import prometheus_client
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 from prometheus_client import Gauge
@@ -13,7 +14,8 @@ app = FastAPI()
 # Disable unwanted metrics
 environ.setdefault("PROMETHEUS_DISABLE_CREATED_SERIES", "True")
 
-DEFAULT_SINCE_SECONDS = 20 * 60
+DEFAULT_SINCE_MINUTES = 15
+ALLOWED_STATS_MINUTES = [0, 15, 22, 30, 45]
 
 
 @app.get("/")
@@ -21,6 +23,7 @@ def read_root():
     return {"Presearch": "Exporter"}
 
 
+NAMESPACE = "pre"
 LABELS = ["node_id", "node_description", "node_url", "gateway_pool", "remote_addr", "version"]
 PERIOD_STATS_NAMES = [
     "total_uptime_seconds",
@@ -40,19 +43,39 @@ PERIOD_STATS_NAMES = [
 ]
 
 
-@app.get("/metrics")
-def metrics(token: str, since_seconds: int = DEFAULT_SINCE_SECONDS):
-    # Registry per request
+def create_registry(stats):
     registry = prometheus_client.CollectorRegistry(auto_describe=True)
-    pre_info = Gauge('pre_info', 'Description of gauge', labelnames=LABELS, registry=registry)
-    pre_connected = Gauge('pre_connected', 'Description of gauge', labelnames=["node_id"], registry=registry)
-    pre_blocked = Gauge('pre_blocked', 'Description of gauge', labelnames=["node_id"], registry=registry)
-    pre_connections = Gauge('pre_connections', 'Description of gauge', labelnames=["node_id"], registry=registry)
-    pre_disconnections = Gauge('pre_disconnections', 'Description of gauge', labelnames=["node_id"], registry=registry)
-    period_stats = [
-        Gauge(f'pre_{stat}', 'Description of gauge', labelnames=["node_id"], registry=registry)
-        for stat in PERIOD_STATS_NAMES
-    ]
+    node_info = Gauge('node_info', 'Description', labelnames=LABELS, namespace=NAMESPACE, registry=registry)
+    connected = Gauge('connected', 'Description', labelnames=["node_id"], namespace=NAMESPACE, registry=registry)
+    blocked = Gauge('blocked', 'Description', labelnames=["node_id"], namespace=NAMESPACE, registry=registry)
+    main_metrics = [node_info, connected, blocked]
+    if stats:
+        connections = Gauge(
+            'connections', 'Description', labelnames=["node_id"], namespace=NAMESPACE, registry=registry
+        )
+        disconnections = Gauge(
+            'disconnections', 'Description', labelnames=["node_id"], namespace=NAMESPACE, registry=registry
+        )
+        period_stats = [
+            Gauge(stat, 'Description', labelnames=["node_id"], namespace=NAMESPACE, registry=registry)
+            for stat in PERIOD_STATS_NAMES
+        ]
+        stats_metrics = [connections, disconnections, period_stats]
+    else:
+        stats_metrics = [None, None, []]
+    return registry, main_metrics, stats_metrics
+
+
+@app.get("/metrics")
+def metrics(token: str):
+    # We only fetch stats at minutes 0 15 30 45
+    utcnow = datetime.utcnow()
+    stats = utcnow.minute in ALLOWED_STATS_MINUTES
+
+    # Registry and set of metrics per request
+    registry, main_metrics, stats_metrics = create_registry(stats=stats)
+    node_info, connected, blocked = main_metrics
+    connections, disconnections, period_stats = stats_metrics
 
     # import json
     # with open("example.json") as f:
@@ -60,11 +83,13 @@ def metrics(token: str, since_seconds: int = DEFAULT_SINCE_SECONDS):
 
     # Fetch stats from API
     url = f"https://nodes.presearch.org/api/nodes/status/{token}"
-    start_date = datetime.utcnow() - timedelta(seconds=since_seconds)
-    params = {
-        "start_date": start_date.strftime("%Y-%m-%d %H:%M"),
-        "stats": "true"
-    }
+    params = {}
+    if stats:
+        start_date = utcnow - timedelta(minutes=DEFAULT_SINCE_MINUTES)
+        params = {
+            "start_date": start_date.strftime("%Y-%m-%d %H:%M"),
+            "stats": "true"
+        }
     print(f"Requesting {url=}, {params=}")
     res = get(url=url, params=params)
     data = res.json()
@@ -85,12 +110,17 @@ def metrics(token: str, since_seconds: int = DEFAULT_SINCE_SECONDS):
             "remote_addr": node["meta"]["remote_addr"],
             "version": node["meta"]["version"],
         }
-        pre_info.labels(**labels).set(1)
-        pre_connected.labels(node_id).set(1 if node["status"]["connected"] else 0)
-        pre_blocked.labels(node_id).set(1 if node["status"]["blocked"] else 0)
-        pre_connections.labels(node_id).set(node["period"]["connections"]["num_connections"] or 0)
-        pre_disconnections.labels(node_id).set(node["period"]["disconnections"]["num_disconnections"] or 0)
-        for stat_name, stat_metric in zip(PERIOD_STATS_NAMES, period_stats):
-            stat_metric.labels(node_id).set(node["period"][stat_name] or 0)
+        node_info.labels(**labels).set(1)
+        connected.labels(node_id).set(1 if node["status"]["connected"] else 0)
+        blocked.labels(node_id).set(1 if node["status"]["blocked"] else 0)
+        if stats:
+            connections.labels(node_id).set(node["period"]["connections"]["num_connections"] or 0)
+            disconnections.labels(node_id).set(node["period"]["disconnections"]["num_disconnections"] or 0)
+            for stat_name, stat_metric in zip(PERIOD_STATS_NAMES, period_stats):
+                stat_metric.labels(node_id).set(node["period"][stat_name] or 0)
 
     return PlainTextResponse(prometheus_client.generate_latest(registry))
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
